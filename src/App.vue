d092e981271596622af413d87dd01814bde68ae7
Bill Du <799552934@qq.com>
2020-08-10 11:13:23 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <div id="app">
-    
+    <router-view></router-view>
+    <main-tab-bar/>
   </div>
 </template>
 
-<style>
+<script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
 
+export default {
+  name:'app',
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+@import url(./assets/css/base.css);
+
+#tab-bar{
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.tab-bar-item{
+  flex: 1;
+  text-align: center;
+  background-color: #f5f4f6;
+  height: 49px;
+  font-size: 14px;
+  padding-top: 3px;
+}
+.tab-bar-item img{
+    width: 24px;
+    vertical-align: middle;
+}
+.active{
+  color: #d81e06;
+}
 </style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,38 @@
+<template>
+     <div class="tab-bar-item" @click="itemClick">
+       <div v-if="!isActive"><slot name="item-icon"></slot></div>
+       <div v-else><slot name="item-icon-active"></slot></div>
+       <div :class="{active:isActive}"><slot name="item-text"></slot></div>
+     </div>
+</template>
+<script>
+export default {
+  name:'TabBarItem',
+  data() {
+    return {
+    }
+  },
+  computed: {
+    isActive(){
+      return this.$route.path.indexOf(this.path) !==-1
+    }
+  },
+  props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+  methods: {
+    itemClick(){
+      this.$router.replace(this.path)
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+
+</style>
